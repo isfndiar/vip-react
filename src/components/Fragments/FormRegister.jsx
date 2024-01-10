cad984651bfcc12d9b3a6123fba2876856da332c
isfandiar adi <141855809+isfndiar@users.noreply.github.com>
2024-01-10 16:37:54 +0700

    form register useref

--- src/components/Fragments/FormRegister.jsx
+++ src/components/Fragments/FormRegister.jsx
@@ -1,29 +1,60 @@
-import InputForm from '../Elements/Input'
-import Button from '../Elements/Button'
-import SwitchPage from '../Elements/SwitchAccount';
+import InputForm from "../Elements/Input";
+import Button from "../Elements/Button";
+import SwitchPage from "../Elements/SwitchAccount";
+import { useEffect, useRef } from "react";
 
 function FormRegister(props) {
-    const {text} = props;
-    
-    const handleLogin = e => {
-      e.preventDefault()
+  const { text } = props;
 
-      localStorage.setItem('email', e.target.email.value)
-      localStorage.setItem('fullname', e.target.text.value)
-      localStorage.setItem('password', e.target.password.value)
-      localStorage.setItem('confirm-password', e.target.confirmPassword.value)
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-      window.location.href = '/products'
-    }
+    localStorage.setItem("email", e.target.email.value);
+    localStorage.setItem("fullname", e.target.text.value);
+    localStorage.setItem("password", e.target.password.value);
+    localStorage.setItem("confirm-password", e.target.confirmPassword.value);
+
+    window.location.href = "/products";
+  };
+
+  const fullNameRef = useRef(null);
+
+  useEffect(() => {
+    fullNameRef.current.focus();
+  }, []);
+
   return (
-    <form action="" onSubmit={handleLogin} >
-    <InputForm label="Full Name" name="text" type="text" placeholder="type your name" />
-    <InputForm label="Email" name="email" type="email" placeholder="example@gmail.com" />
-    <InputForm label="Password" name="password" type="password" placeholder={`********`} />
-    <InputForm label="Confirm Password" name="confirmPassword" type="password" placeholder={`********`} />
-    <Button classname="bg-blue-600 w-full" type='submit'>{text}</Button>
-  </form>
-  )
+    <form action="" onSubmit={handleLogin}>
+      <InputForm
+        label="Full Name"
+        name="text"
+        type="text"
+        placeholder="type your name"
+        ref={fullNameRef}
+      />
+      <InputForm
+        label="Email"
+        name="email"
+        type="email"
+        placeholder="example@gmail.com"
+      />
+      <InputForm
+        label="Password"
+        name="password"
+        type="password"
+        placeholder={`********`}
+      />
+      <InputForm
+        label="Confirm Password"
+        name="confirmPassword"
+        type="password"
+        placeholder={`********`}
+      />
+      <Button classname="bg-blue-600 w-full" type="submit">
+        {text}
+      </Button>
+    </form>
+  );
 }
 
-export default FormRegister
+export default FormRegister;
