146d72f3ee8361ba2625e4331bf1dc2ce48c136b
isfandiar adi <141855809+isfndiar@users.noreply.github.com>
2024-01-27 10:35:35 +0700

    ksadfjsadfjkl

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,10 +4,15 @@ import useLogin from "../hooks/useLogin";
 export default function ProfilePage() {
   const username = useLogin(); // Login Token
 
+  const onScroll = () => {};
   return (
-    <div>
-      Page
-      <div>username : {username}</div>
+    <div className="bg-black text-white overflow-x-auto snap-x  whitespace-nowrap    ">
+      <span className="h-screen w-screen text-black bg-white flex justify-center items-center  ">
+        Lorem
+      </span>
+      <span className=" h-screen w-screen flex justify-center items-center ">
+        ipsum
+      </span>
     </div>
   );
 }
